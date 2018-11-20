74b39efbc698e10e16e8e21e7993b8b76cacd4c4
Mike V <dev.mikev@gmail.com>
2018-11-20 10:55:04 -0500

    commit src/ folder

---
+++ src/com/voloshin/BankAccount.java
@@ -0,0 +1,45 @@
+package com.voloshin;
+
+public class BankAccount {
+
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
+
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int typeOfAccount) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+        this.accountType = typeOfAccount;
+    }
+
+    // The branch arguemnet is true if the customer is performing the transaction
+    // at a branch with a teller.
+    // It's false if the customer is performing the transaction at an ATM
+    public double deposit(double amount, boolean branch) {
+        balance += amount;
+        return balance;
+    }
+
+    // The branch arguemnet is true if the customer is performing the transaction
+    // at a branch with a teller.
+    // It's false if the customer is performing the transaction at an ATM
+    public double withdrawal(double amount, boolean branch) {
+        balance -= amount;
+        return balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
+    }
+
+}
